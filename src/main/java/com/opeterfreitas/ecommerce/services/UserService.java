041eb8f6aa9ac7b84825f9b6c795c69f210c005e
Peter Freitas <opeterfreitas@gmail.com>
2022-05-19 16:57:41 -0300

    Exception handling - findById

--- src/main/java/com/opeterfreitas/ecommerce/services/UserService.java
+++ src/main/java/com/opeterfreitas/ecommerce/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.opeterfreitas.ecommerce.entities.User;
 import com.opeterfreitas.ecommerce.repositories.UserRepository;
+import com.opeterfreitas.ecommerce.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -22,7 +23,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
@@ -32,19 +33,19 @@ public class UserService {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
-	
+
 	public User update(Long id, User obj) {
 		User entity = repository.getOne(id);
 		updateData(entity, obj);
 		return repository.save(entity);
-		
+
 	}
 
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-		
+
 	}
-	
+
 }
