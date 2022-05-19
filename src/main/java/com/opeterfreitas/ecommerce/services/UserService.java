86631bd354543d6da7734ffa7da19c7188b0a23d
Peter Freitas <opeterfreitas@gmail.com>
2022-05-19 15:39:38 -0300

    User insert

--- src/main/java/com/opeterfreitas/ecommerce/services/UserService.java
+++ src/main/java/com/opeterfreitas/ecommerce/services/UserService.java
@@ -19,10 +19,14 @@ public class UserService {
 		return repository.findAll();
 
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
+	}
+
+	public User insert(User obj) {
+		return repository.save(obj);
 	}
 
 }
